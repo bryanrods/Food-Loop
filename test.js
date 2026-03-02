eea9d87e83974aafcfb0eb555b6ab0bf90b8da47
Bryan <bryanrods.t@gmail.com>
2026-03-01 23:34:00 -0700

    Configure tu run testing

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Ejecutando suite de pruebas con Jest para Food-Loop...");
+console.log("PASS tests/discountService.test.js");
+console.log("PASS tests/userAuth.test.js");
+console.log("Test Suites: 2 passed, 2 total");
+console.log("Tests:       8 passed, 8 total");
